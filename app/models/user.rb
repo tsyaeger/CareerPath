4d5f33a8f9a3be93bec9aadb3a4de589d16cd384
tsyaeger <tsyaeger@gmail.com>
2018-12-30 21:32:26 -0800

    override devise forms, added username field

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
+ attr_writer :login
+
   has_many :contacts  
   has_many :documents
   has_many :jobs
@@ -9,4 +11,21 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+        where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+    end
+  end
 end
+
+
