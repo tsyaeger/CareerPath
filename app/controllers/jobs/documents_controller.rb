47291fdc4f80bb3013d4d907d9e39a858ace114f
tsyaeger <tsyaeger@gmail.com>
2019-01-03 22:51:11 -0800

    Replace before action with before filter

--- app/controllers/jobs/documents_controller.rb
+++ app/controllers/jobs/documents_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module Jobs
   class DocumentsController < ApplicationController
-    skip_before_action :verify_authenticity_token
+    skip_before_filter :verify_authenticity_token
 
     def unlink
       document = Document.find(params[:document_id])
--- app/controllers/tasks/contacts_controller.rb
+++ app/controllers/tasks/contacts_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module Tasks
   class ContactsController < ApplicationController
-    skip_before_action :verify_authenticity_token
+    skip_before_filter :verify_authenticity_token
 
     def unlink
       contact = Contact.find(params[:contact_id])
